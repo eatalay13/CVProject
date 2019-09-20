b0d5e1687fa229a211d278974e1bedb54bd65c6a
Emrah Atalay <emrahatalay92@gmail.com>
2019-09-20 14:56:52 +0300

    bussines exception eklendi

---
+++ CVP.Infrastructure/BussinesException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CVP.Infrastructure
+{
+    public class BussinesException : Exception
+    {
+        public BussinesException(string message) : base(message)
+        {
+
+        }
+    }
+}
